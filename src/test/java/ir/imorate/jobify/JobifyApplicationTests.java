cc76efa3855b71e843b5fbe6b7b0f410d7cec348
Amir Mohammad Hl <dev.imorate@gmail.com>
2021-11-09 19:25:00 +0330

    Initial commit

---
+++ src/test/java/ir/imorate/jobify/JobifyApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.imorate.jobify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
