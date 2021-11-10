ea91d386b613d2d4e6c12e00da741bc735bdcf67
Amir Mohammad Hl <dev.imorate@gmail.com>
2021-11-10 19:53:31 +0330

    Add app.url properties and fix bug in properties classes

--- src/main/java/ir/imorate/jobify/JobifyApplication.java
+++ src/main/java/ir/imorate/jobify/JobifyApplication.java
@@ -1,12 +1,13 @@
 package ir.imorate.jobify;
 
-import ir.imorate.jobify.config.ApplicationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(ApplicationProperties.class)
+@EnableConfigurationProperties
+@ConfigurationPropertiesScan
 public class JobifyApplication {
 
     public static void main(String[] args) {
--- src/main/java/ir/imorate/jobify/config/properties/ApplicationProperties.java
+++ src/main/java/ir/imorate/jobify/config/properties/ApplicationProperties.java
@@ -1,10 +1,12 @@
-package ir.imorate.jobify.config;
+package ir.imorate.jobify.config.properties;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "app")
 @Getter
+@Setter
 public class ApplicationProperties {
 
     /**
