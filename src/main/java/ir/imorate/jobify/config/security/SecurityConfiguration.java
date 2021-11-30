843155bde3da181b5b8f6be421e741bbe2c8df94
Amir Mohammad Hl <dev.imorate@gmail.com>
2021-11-30 12:40:04 +0330

    Add SecurityConfiguration

--- src/main/java/ir/imorate/jobify/config/security/SecurityConfiguration.java
+++ src/main/java/ir/imorate/jobify/config/security/SecurityConfiguration.java
@@ -1,33 +1,30 @@
 package ir.imorate.jobify.config.security;
 
-import ir.imorate.jobify.config.properties.ControllerProperties;
 import lombok.AllArgsConstructor;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
-@EnableWebSecurity
+import javax.sql.DataSource;
+
+@Configuration
 @AllArgsConstructor
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
-    private final ControllerProperties controllerProperties;
-    private final ControllerProperties.Login loginControllerProperties;
+    private final DataSource dataSource;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/", controllerProperties.getSignup()).permitAll()
-                .antMatchers(loginControllerProperties.getUrl()).permitAll()
-                .anyRequest()
-                .authenticated();
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
-    @Override
-    public void configure(WebSecurity web) {
-        web
-                .ignoring()
-                .antMatchers("/favicon.ico", "/logo.svg", "/style/**", "/media/**");
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        return jdbcTokenRepository;
     }
+
 }
