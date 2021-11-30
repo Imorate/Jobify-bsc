f7766d10338f4424cefbd5f237c0be5381766936
Amir Mohammad Hl <dev.imorate@gmail.com>
2021-11-30 12:53:17 +0330

    Add SecurityController and its view and assets

---
+++ src/main/java/ir/imorate/jobify/controller/SecurityController.java
@@ -0,0 +1,44 @@
+package ir.imorate.jobify.controller;
+
+import ir.imorate.jobify.entity.security.dto.SignupDTO;
+import ir.imorate.jobify.entity.security.enums.RoleType;
+import ir.imorate.jobify.service.security.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+
+@Controller
+@AllArgsConstructor
+public class SecurityController {
+
+    private final UserService userService;
+
+    @GetMapping("/login")
+    public String login(@ModelAttribute("registeredUser") SignupDTO registeredUser) {
+        return "security/login";
+    }
+
+    @GetMapping("/signup")
+    public String signup(Model model) {
+        model.addAttribute("signupDTO", new SignupDTO());
+        return "security/signup";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@Valid @ModelAttribute SignupDTO signupDTO, BindingResult bindingResult,
+                         RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            return "security/signup";
+        }
+        userService.signup(signupDTO, RoleType.ROLE_USER);
+        redirectAttributes.addFlashAttribute("registeredUser", signupDTO);
+        return "redirect:/login";
+    }
+}
