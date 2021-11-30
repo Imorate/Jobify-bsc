17e329d1abf54b93109e3e582d2eecfa68a0b4e0
Amir Mohammad Hl <dev.imorate@gmail.com>
2021-11-30 12:40:37 +0330

    Add StringToGenderConverter

---
+++ src/main/java/ir/imorate/jobify/entity/security/enums/converter/StringToGenderConverter.java
@@ -0,0 +1,17 @@
+package ir.imorate.jobify.entity.security.enums.converter;
+
+import ir.imorate.jobify.entity.security.enums.Gender;
+import org.springframework.core.convert.converter.Converter;
+
+import java.util.Arrays;
+
+
+public class StringToGenderConverter implements Converter<String, Gender> {
+
+    @Override
+    public Gender convert(String source) {
+        return Arrays.stream(Gender.values()).filter(gender -> gender.getTitle().equals(source)).findFirst()
+                .orElse(Gender.OTHER);
+    }
+
+}
