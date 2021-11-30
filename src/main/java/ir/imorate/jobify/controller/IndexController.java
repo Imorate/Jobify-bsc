5f058232662e7c869accda20b59d2b5fe03d82c8
Amir Mohammad Hl <dev.imorate@gmail.com>
2021-11-30 12:51:52 +0330

    Add IndexController and its view

--- src/main/java/ir/imorate/jobify/controller/IndexController.java
+++ src/main/java/ir/imorate/jobify/controller/IndexController.java
@@ -1,13 +1,33 @@
 package ir.imorate.jobify.controller;
 
+import ir.imorate.jobify.entity.security.Profile;
+import ir.imorate.jobify.entity.security.User;
+import ir.imorate.jobify.service.security.ProfileService;
+import ir.imorate.jobify.service.security.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Optional;
+
 @Controller
+@AllArgsConstructor
 public class IndexController {
 
+    private UserService userService;
+    private ProfileService profileService;
+
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        if (userService.isAuthenticated()) {
+            Optional<User> currentUserLogin = userService.getCurrentUserLogin();
+            if (currentUserLogin.isPresent()) {
+                model.addAttribute("currentUser", currentUserLogin.get());
+                Optional<Profile> currentUserProfile = profileService.findProfile(currentUserLogin.get());
+                currentUserProfile.ifPresent(profile -> model.addAttribute("currentUserProfile", profile));
+            }
+        }
         return "index";
     }
 
